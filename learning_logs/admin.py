4c34374df2f39697143f50f67a5286bde83d4029
wgh-12400 <3030649311@qq.com>
2024-07-26 15:17:55 +0800

    added model entry

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # 在admin.py 所在的⽬录中查找 models.py
-from .models import Topic
+from .models import Topic, Entry
 
 
 admin.site.register(Topic)
+admin.site.register(Entry)
