cdf8571404548454462260565c903d991e31b801
wgh-12400 <3030649311@qq.com>
2024-07-26 11:08:17 +0800

    activated model

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 自定义app，放在默认app前能够覆盖默认app行为
+    'learning_logs',
+
+    # django默认app
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
